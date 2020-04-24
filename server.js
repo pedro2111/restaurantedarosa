c88fad8e7b9da1384f805aa1342825c4f5021e83
Pedro Chaves <pedrohbchaves@gmail.com>
2020-04-24 19:18:51 -0300

    server js cors2

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const path = require('path');
 const app = express();
 app.use(cors());
 
-app.use(function (req,res,next) {
+//CORS Middleware
 
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization');
     next();
-
-});
+    });
+    
 
 app.use(express.static(__dirname + '/dist/cozinha-da-rosa'));
 app.get('/*', function(req,res) {
