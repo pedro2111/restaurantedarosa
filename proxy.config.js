c39adde71dd44d6a7215029944cb560adab91cc6
Pedro Chaves <pedrohbchaves@gmail.com>
2020-04-24 19:46:59 -0300

    server js cors4

--- proxy.config.js
+++ proxy.config.js
@@ -2,7 +2,7 @@ const proxy = [
     {
       context: '/api',
       //target: 'http://localhost:8080',
-      target: 'https://cozinhadarosa.herokuapp.com:8080',
+      target: 'https://cozinhadarosa.herokuapp.com',
       pathRewrite: {'^/api' : ''},
       secure: false,
       changeOrigin: true
--- src/app/shared/app.api.ts
+++ src/app/shared/app.api.ts
@@ -1,2 +1,2 @@
-export const URL_API = 'https://cozinhadarosa.herokuapp.com:8080/api';
+export const URL_API = 'https://restaurantedarosa.herokuapp.com/api';
 //export const URL_API = 'http://localhost:4200/api';
