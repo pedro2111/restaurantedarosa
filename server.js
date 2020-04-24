208143303a42912d397a6580c2f093a27f2bc332
Pedro Chaves <pedrohbchaves@gmail.com>
2020-04-24 19:06:50 -0300

    server js cors

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 app.use(cors());
+
+app.use(function (req,res,next) {
+
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+});
+
 app.use(express.static(__dirname + '/dist/cozinha-da-rosa'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
