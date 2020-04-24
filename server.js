c2e9acad966c983d331dfb5223feaf6536a2672f
Pedro Chaves <pedrohbchaves@gmail.com>
2020-04-24 00:05:20 -0300

    cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
+app.use(cors());
 app.use(express.static(__dirname + '/dist/cozinha-da-rosa'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
--- src/app/shared/app.api.ts
+++ src/app/shared/app.api.ts
@@ -1,2 +1,2 @@
-export const URL_API = 'https://cozinhadarosa.herokuapp.com';
-//export const URL_API = 'http://localhost:4200/api';
+//export const URL_API = 'https://cozinhadarosa.herokuapp.com';
+export const URL_API = 'http://localhost:4200/api';
