fa0764da4ea120a1f526241360edcb22a293dd94
Pedro Chaves <pedrohbchaves@gmail.com>
2020-04-25 14:44:37 -0300

    cors6

--- src/app/shared/app.api.ts
+++ src/app/shared/app.api.ts
@@ -1,2 +1,3 @@
-export const URL_API = 'https://restaurantedarosa.herokuapp.com/api';
+export const URL_API = 'https://cozinhadarosa.herokuapp.com';
+//export const URL_API = 'https://restaurantedarosa.herokuapp.com/api';
 //export const URL_API = 'http://localhost:4200/api';
